afaf93e8fd6ed4c386130fa201a3b942df5e4893
Da Yeong <yoon351200@naver.com>
2023-01-18 17:14:47 +0900

    stack_10828

---
+++ Week2_DataStructure/zero_10773.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+s = []
+for i in range(n):
+    tmp = int(sys.stdin.readline().strip())
+    if tmp == 0:
+        s.pop()
+    else:
+        s.append(tmp)
+print(sum(s))
