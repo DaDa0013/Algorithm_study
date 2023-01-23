0b0ca89d00c78078f5fe8e1627cdc181046428c8
Da Yeong <yoon351200@naver.com>
2023-01-23 17:22:38 +0900

    큐_10845

---
+++ Week2_DataStructure/queue_10845.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+q = deque()
+for i in range(int(sys.stdin.readline())):
+    m = sys.stdin.readline().split()
+    if m[0] == 'push':
+        q.append(int(m[1]))
+    elif m[0] == 'pop':
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif m[0] == 'size':
+        print(len(q))
+    elif m[0] == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif m[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    else:
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
