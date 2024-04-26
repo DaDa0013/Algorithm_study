9ca492fda0fc2a14c4f8e02110751940f6ce974d
Yoon Da Young <79195793+DaDa0013@users.noreply.github.com>
2024-04-26 22:04:11 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
@@ -0,0 +1,22 @@
+N = int(input())
+pattern = input()
+s = pattern.index('*')
+prev = pattern[:s]
+suf = pattern[s+1:]
+files = [list(input()) for _ in range(N)]
+for file in files:
+    if len(file) >= len(prev) + len(suf):
+        if len(file) > len(prev):
+            check = 'DA'
+            for i in range(len(prev)): # * 앞
+                if file[i] != prev[i]:
+                    check = 'NE'
+                    break
+            if check == 'DA' and len(file) > len(suf):
+                for i in range(-1, -len(suf) - 1, -1):  # * 뒤
+                    if file[i] != pattern[i]:
+                        check = 'NE'
+                        break
+            print(check)
+    else:
+        print('NE')
